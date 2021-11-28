87a1bb178aad2f41e4d0bbda39ebcfb17755b88e
Adam Valiska <adamvaliska@valiska.eu>
2021-11-28 19:57:31 +0100

    ok

---
+++ lib/models/lots_item.dart
@@ -0,0 +1,12 @@
+class LotsItem {
+  int ID;
+  double lat;
+  double lon;
+  String name;
+  String lore;
+  int user_id;
+  String place_special_ID;
+  int is_on_open_place;
+  String contact_phone;
+  String contact_email;
+}
