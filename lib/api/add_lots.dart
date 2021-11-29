65057c291bb67b109a1c37ebeb2d5baa65f1cb85
Adam Valiska <adamvaliska@valiska.eu>
2021-11-29 15:13:19 +0100

    update

--- lib/api/add_lots.dart
+++ lib/api/add_lots.dart
@@ -6,21 +6,17 @@ String lotsAddModelToJson(LotsAddModel data) => json.encode(data.toJson());
 
 class LotsAddModel {
   int code = 0;
-  List data;
 
   LotsAddModel({
     this.code,
-    this.data,
   });
 
   factory LotsAddModel.fromJson(Map<String, dynamic> json) => LotsAddModel(
         code: json["code"],
-        data: json["data"] == null ? null : json["data"],
       );
 
   Map<String, dynamic> toJson() =>
       {
         "code": code,
-        "data": data == null ? null : data,
       };
 }
